e7d94ee8cecc475116255147dbd683046c4b8c00
Ana onablo <79449819+onablo@users.noreply.github.com>
2021-03-26 20:29:18 +0200

    js-demo

---
+++ js/components/matematika.js
@@ -0,0 +1,20 @@
+const sum = (a, b) => a + b;
+const minus = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+export {
+    sum,
+    minus,
+    multiply,
+    divide
+}
+
+/*const matematika = {
+    sudeti: sum,
+    atimti: minus,
+    sudauginti: multiply ,
+    dalinti:divide
+}
+export {matematika}
+*/
---
+++ js/main-demo-page.js
@@ -0,0 +1,23 @@
+/*import { suma } from './components/sum.js';
+import  {multiply as daugyba } from './components/multiply.js';
+*/
+
+import {sum, minus, multiply, divide } from './components/matematika.js';
+
+const rez1 = sum(8, 5);
+const rez2 = minus(8, 5);
+const rez3 = multiply(8, 5);
+const rez4 = divide(8, 5);
+
+console.log(rez1);
+console.log(rez2);
+console.log(rez3);
+console.log(rez4);
+
+/* import {matematika} from 'components/matematika.js';
+
+const rez1 = matematika.sudeti (8, 5);
+const rez1 = matematika.atimti (8, 5);
+const rez1 = matematika.sudauginti (8, 5);
+const rez1 = matematika.dalinti (8, 5);
+*/
